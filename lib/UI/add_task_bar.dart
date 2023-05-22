8f3b0b3a9b809b40e19fb71ff5d120d1c72d9012
Van Cong Bao <congbao7201@gmail.com>
2023-05-22 10:44:13 +0700

    Update

--- lib/UI/add_task_bar.dart
+++ lib/UI/add_task_bar.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/UI/theme.dart';
+import 'package:flutter_application_1/UI/widget/input_field.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({super.key});
 
   @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.theme.colorScheme.background,
       appBar: _appBar(context),
       body: Container(
         padding: const EdgeInsets.only(left: 20, right: 20),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Add task",
-              style: headingStyle,),
-              
-              ],
+              Text(
+                "Add task",
+                style: headingStyle,
+              ),
+              MyInputField(title: "Title", hint: "Enter your title"),
+              MyInputField(title: "Note", hint: "Enter your note"),
+              MyInputField(
+                  title: "Date", hint: DateFormat.yMd().format(_selectedDate)),
+            ],
           ),
         ),
       ),
@@ -28,7 +43,7 @@ class AddTaskPage extends StatelessWidget {
   _appBar(BuildContext context) {
     return AppBar(
       elevation: 0,
-      backgroundColor: context.theme.backgroundColor,
+      backgroundColor: context.theme.colorScheme.background,
       leading: GestureDetector(
         onTap: () {
           Get.back();
